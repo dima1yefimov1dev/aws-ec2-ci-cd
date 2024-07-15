5654661f4390a0f97fc253bd472ad8fbb2b40e5f
dima1yefimov1dev <162560416+dima1yefimov1dev@users.noreply.github.com>
2024-07-15 07:03:33 +0300

    divided dev ci to seperate jobs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from ьу!';
+    return process.env.HELLO_MESSAGE;
   }
 }
