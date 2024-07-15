1813fffab4acc955804c604bf41900d929dcd678
dima1yefimov1dev <162560416+dima1yefimov1dev@users.noreply.github.com>
2024-07-15 07:54:36 +0300

    fix service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return process.env.HELLO_MESSAGE;
+    return `${process.env.HELLO_MESSAGE}`;
   }
 }
